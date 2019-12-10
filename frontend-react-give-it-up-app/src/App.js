dcd1232c3b36fcbe357ed976be84d08e93cf9bdf
github username <github email address>
2019-12-10 11:34:08 -0500

    refactered cartAction

--- frontend-react-give-it-up-app/src/App.js
+++ frontend-react-give-it-up-app/src/App.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import SigninPage from './containers/SigninPage'
-import NavBar from './components/NavBar'
-import HomePage from './containers/HomePage'
-import Cart from './components/Cart'
-import UserProfile from './components/UserProfile'
-import ViceContainer from './containers/ViceContainer'
+import SigninPage from "./containers/SigninPage";
+import NavBar from "./components/NavBar";
+import HomePage from "./containers/HomePage";
+import Cart from "./containers/Cart";
+import UserProfile from "./components/UserProfile";
+import ViceContainer from "./containers/ViceContainer";
 
 class App extends React.Component {
-
   render() {
     return (
       <React.Fragment>
         <NavBar />
 
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/cart" component={Cart}/>
-          <Route path="/signup" component={SigninPage}/>
-          <Route path="/profile" component={UserProfile}/>
-          <Route path="/vices" component={ViceContainer}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/signup" component={SigninPage} />
+          <Route path="/profile" component={UserProfile} />
+          <Route path="/vices" component={ViceContainer} />
         </Switch>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
