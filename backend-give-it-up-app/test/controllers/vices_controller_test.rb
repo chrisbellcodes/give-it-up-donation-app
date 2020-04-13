12421792c921230e2a748a10b2c3410e895403d5
Chris Bell <github email address>
2020-04-12 22:08:38 -0400

    feat(*): config testing
    Remove test files and add the following to for testing:
    
    -Capybara
    -factory_girl
    -database_cleaner
    -rspec config

--- backend-give-it-up-app/test/controllers/vices_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class VicesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- backend-give-it-up-app/test/models/vice_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ViceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
