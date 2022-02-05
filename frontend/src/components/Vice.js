c18190e14eef91bbcd5c56f437eab4cd374d42b2
Chris Bell <chrisbellcodes@gmail.com>
2022-02-05 17:16:56 -0500

    (vice compnent): restyle

--- frontend/src/components/Vice.js
+++ frontend/src/components/Vice.js
@@ -2,22 +2,27 @@ import React from 'react'
 import '../App.css';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
 
 const Vice = (props) => {
-  const { name, amount } = props
+  console.log(props)
+  const { name, description, amount } = props
 
   return(
-    <Card bg="dark" text="light" style={{ "min-width": '200px' }} as={Col} xs={2} className="mx-2 my-4">
-      <Card.Img as={Image} variant="top" src="logo192.png" />
-      <Card.Body>
-        <Card.Title className="cardtext">{name}</Card.Title>
-      </Card.Body>
-      <Card.Footer>
-      <Card.Text className="cardtext"> ${amount} </Card.Text>
-      <Button onClick={() => props.handleClick(props)} variant="info">Give this Up</Button>
-      </Card.Footer>
+    <Card className="vice-card">
+      <div className="vice-card__bg"></div>
+      <div className="vice-card-content">
+        <div className="vice-card__img-container">
+          <img className="vice-card__img" src="logo192.png" />
+        </div>
+        <div className="vice-card__info">
+          <h2 className="vice-card__name">{name}</h2>
+          <p className="vice-card__desc">{description}</p>
+        </div>
+      </div>
+      <div className="vice-card__price-btn-conatiner">
+        <span className="vice-card__price"> ${amount} </span>
+        <Button className="vice-card__btn" onClick={() => props.handleClick(props)} variant="info">Give this Up</Button>
+      </div>
     </Card>
 
   )
