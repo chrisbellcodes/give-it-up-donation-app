efdf605de454b760fea4a426ace554998fdde5c4
Chris Bell <chrisbellcodes@gmail.com>
2022-10-04 15:18:24 -0400

    Upgrade to Rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -2,30 +2,29 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_211255) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_190818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "jwt_blacklists", force: :cascade do |t|
     t.string "jti"
-    t.datetime "exp"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "exp", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["jti"], name: "index_jwt_blacklists_on_jti"
   end
 
@@ -35,8 +34,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_211255) do
     t.bigint "vice_id"
     t.integer "quantity"
     t.string "stripe_sub_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
     t.index ["vice_id"], name: "index_subscriptions_on_vice_id"
   end
@@ -45,13 +44,13 @@ ActiveRecord::Schema.define(version: 2020_04_27_211255) do
     t.string "first_name"
     t.string "last_name"
     t.string "stripe_customer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -63,8 +62,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_211255) do
     t.string "stripe_plan_id"
     t.string "stripe_plan_interval"
     t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["category_id"], name: "index_vices_on_category_id"
   end
 
