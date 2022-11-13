71f9f91accb93bb986d195823b6329095bbadb3d
Chris Bell <chrisbellcodes@gmail.com>
2022-11-13 10:42:14 -0500

    (Footer): - Add Footer logo and info - Update styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+
 const Footer = () => {
 
 
 
     return(
         <footer className="main-footer">
-            <div>
+            <div className='Footer__text'>
+                Need help? Have feedback? Email us at <a className="footer-link" target={"_blank"} href="mailto:info@developingarts.org">info@developingarts.org</a>
+            </div>
+            <img className='Footer__da-logo' src={'./da-logo-white.png'} alt='DA logo' />
+            <a className="footer-link" target={"_blank"} href="https://www.developingartists.org/">www.developingartists.org</a>
+            <div className='Footer__text'>
                 © Copyright 2022   |   ALL RIGHTS RESERVED
             </div>
-            <a className="footer-link" target={"_blank"} href="https://www.developingartists.org/">www.developingartists.org</a>
         </footer>
     )
 }
